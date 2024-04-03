193b0b18ab4d1c644ddf35dcd9e1e8bf2d6678c0
apply-root1234 <ruth.recalde@applydigital.com>
2024-04-03 14:08:16 -0400

    CustomCommands_week3_forReview

---
+++ cypress/e2e/tests/verifyCustomCommands.cy.js
@@ -0,0 +1,18 @@
+import commands from "../../support/commands";
+/*
+- Create a Cypress custom command to Login the following website: https://naveenautomationlabs.com/opencart/index.php?route=account/login 
+- After login, search for the item: Macbook, on the search bar. 
+- Create a Cypress custom command to Logout from the website.
+- Use credentials: applytesters@mydomain.com / Apply123!!!
+- Login, Search and Logout must be called on the same TC (4 - 5 Lines Max).
+
+*/
+describe('Login Test', () => {
+    it('Click and type on the email and password fields', () => 
+    {
+      cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
+      cy.login('applytesters@mydomain.com','Apply123!!!');
+      cy.search('Macbook');
+      cy.logout();
+    });
+  });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,23 @@
+import 'cypress-file-upload';
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.get(`input[id="input-email"]`).type(email);
+  cy.get(`input[id="input-password"]`).type(password);
+  cy.get(`input[type="submit"]`).click();
+  
+});
+
+Cypress.Commands.add('search', (term) => {
+  cy.get(`input[type="text"]`).type(term);
+  cy.get(`span[class="input-group-btn"]`).click();
+});
+
+Cypress.Commands.add('logout', () => {
+  // 1. ensure the dropdown is open
+  cy.get('a[href="https://naveenautomationlabs.com/opencart/index.php?route=account/logout"]').parents('.list-inline').click();
+  
+});
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,4 +43,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import 'cypress-file-upload';
